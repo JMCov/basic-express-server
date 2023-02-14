aa7a88864f2198fa93307dea828395e240951959
Jordan Covington <Atlantafan1087@gmail.com>
2023-02-14 14:08:01 -0800

    Lab 2 Complete

---
+++ src/middleware/validator.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const validator = (req, res, next) => {
+
+  if (!req.query.name){
+    throw new Error('Name is required!!');
+
+  } else  {
+    next();
+  }
+
+};
+
+module.exports = validator;
+
